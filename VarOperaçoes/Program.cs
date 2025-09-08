058315bd8034cd7dcca0183b02c95692c511a3b8
Cyberibeiro <beatrizribeiro.milli@gmail.com>
2025-09-08 20:57:58 -0300

    Acrescentei uma pasta

--- VarOperaçoes/Program.cs
+++ VarOperaçoes/Program.cs
@@ -6,6 +6,6 @@ int n1 = int.Parse(Console.ReadLine());
 Console.Write("Digite o segundo número inteiro: ");
 int n2 = int.Parse(Console.ReadLine());
 
-Console.WriteLine($"\nA soma de {n1} + {{n2}} é igual à {n1 + n2}");
+Console.WriteLine($"\nA soma de {n1} +{n2} é igual à {n1 + n2}");
 
 Console.ReadKey();
