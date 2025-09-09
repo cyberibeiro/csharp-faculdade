2148f8f7da5c85cc8a1a107040ec21d0ae2e7efe
Cyberibeiro <beatrizribeiro.milli@gmail.com>
2025-09-08 21:46:49 -0300

    Adivinhe o número

---
+++ AdivinhaNumero/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+Console.Write("Adivinhe o número: ");
+var number = int.Parse(Console.ReadLine()!);
+
+Random numberAleatorio = new();
+
+int num = numberAleatorio.Next(1, 20);
+
+while (number != num)
+{
+    if (number > num)
+    {
+        Console.WriteLine($"\nO {number} é maior que o número aleatório, tente novamente!");
+            
+    }
+    else if (number > num)
+    {
+        Console.WriteLine($"O {number} é menor que o número aleatório, tente novamente!");
+    }
+    else
+    {
+        Console.WriteLine($"Você acertou! O número aleatório é {num}.");
+    }
+}
+
+
+Console.ReadKey();
+
+
+#region (Outra forma)
+//var numberAleatorio = new Randon();
+#endregion
